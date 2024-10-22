1f7703360f63a041c4a5b8d254f66e64b821bcb4
Xiwen Zhang <xiwen.zhang.1995@gmail.com>
2024-10-22 15:33:41 +0800

    change sphinx html theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,5 +28,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# html_theme = 'alabaster'
 html_static_path = ['_static']
