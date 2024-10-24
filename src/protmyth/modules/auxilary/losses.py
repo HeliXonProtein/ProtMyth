c0458648a670144debca34738abb7816ca884c90
fan <ding274@purdue.edu>
2024-10-24 17:04:26 +0800

    [add] compute robertLMHead loss

--- src/protmyth/modules/auxilary/losses.py
+++ src/protmyth/modules/auxilary/losses.py
@@ -15,6 +15,7 @@ from jaxtyping import Float
 
 from protmyth.modules.base import BaseModule
 from protmyth.modules.register import register_module
+from protmyth.modules.common import mask
 
 
 @register_module("losses")
@@ -181,3 +182,34 @@ class ContactPredictionHead(nn.Module):
         attentions = apc(symmetrize(attentions))
         attentions = attentions.permute(0, 2, 3, 1)
         return self.activation(self.regression(attentions).squeeze(3))
+
+
+def compute_RobertaLMHead_loss(
+        logits: Float[torch.Tensor, "..."],
+        label: Float[torch.Tensor, "..."],
+        label_mask: Float[torch.Tensor, "..."],
+        ) -> Float[torch.Tensor, "..."]:
+    """compute mask LM loss.
+
+    Parameters
+    ----------
+    logits: Float[torch.Tensor, "..."]
+        A tensor containing the logits.
+    label: Float[torch.Tensor, "..."]
+        A tensor containing the label.
+    label_mask: Float[torch.Tensor, "..."]
+        A tensor containing the label_mask.
+    Returns
+    -------
+    torch.Tensor
+        A tensor of batch loss
+    """
+
+    # CrossEntropyLoss
+    with torch.no_grad():
+        label = label.long()
+        label = torch.where(label_mask == 1, label, (torch.ones_like(label) * (-100)).type_as(label))
+    loss_fn = nn.CrossEntropyLoss(ignore_index=-100, reduction='none')
+    loss = loss_fn(logits, label)
+    loss = mask.masked_mean(value=loss, mask=label_mask, dim=(-1, -2))
+    return loss
